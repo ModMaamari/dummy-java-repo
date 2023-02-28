360070820a7543bbbe5c995be279161e23920392
al-maamari <almaam01@ads.uni-passau.de>
2023-02-28 10:23:27 +0100

    initial

---
+++ src/dummy1.java
@@ -0,0 +1,13 @@
+public class dummy1 {
+    public static void myFirstFunc(){
+        // hacy
+        int a = 5;
+
+    }
+
+    public static int mysecondFunc(){
+        // this function is stupid
+        int a = 66;
+        return a;
+    }
+}
